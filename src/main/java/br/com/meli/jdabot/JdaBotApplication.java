d8823e7be8751148458858475dc59eebf94385da
Fernando Rosa <f.rosaoliveira@gmail.com>
2025-07-31 13:55:46 -0300

    Primeiro commit

---
+++ src/main/java/br/com/meli/jdabot/JdaBotApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.jdabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdaBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/jdabot/JdaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.jdabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
