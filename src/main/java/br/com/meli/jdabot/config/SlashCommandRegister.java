aa1af42d83f5494c94af9337f5707eb24a910e96
Fernando Rosa <f.rosaoliveira@gmail.com>
2025-08-05 19:33:43 -0300

    criado e att SlashCommandRegister

---
+++ src/main/java/br/com/meli/jdabot/config/SlashCommandRegister.java
@@ -0,0 +1,31 @@
+package br.com.meli.jdabot.config;
+
+
+import jakarta.annotation.PostConstruct;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SlashCommandRegister {
+
+    private final JDA jda;
+    private final String guildId;
+
+    public SlashCommandRegister(JDA jda, @Value("${discord.guild.id}") String guildId) {
+        this.jda = jda;
+        this.guildId = guildId;
+    }
+
+    @PostConstruct
+    public void registerCommands() {
+        jda.getGuildById(guildId).updateCommands().addCommands(
+                Commands.slash("ping", "Testa o bot"),
+                Commands.slash("clima", "Consulta o clima pelo CEP")
+                        .addOption(OptionType.STRING, "cep", "Digite o CEP", true),
+                Commands.slash("Questionario", "Seleciona questionários usando filtros")
+        ).queue();
+    }
+}
